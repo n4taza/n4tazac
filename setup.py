a7b636793695d0b780f03e66ad0208e07d46cf33
n4taza <accguest88080@gmail.com>
2026-05-14 08:01:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import glob
 
 setup(
     name="glitxh",
-    version="0.1.4",
+    version="0.1.5",
 
     package_dir={"": "src"},
     packages=find_packages(where="src"),
 
     ext_modules=cythonize(
-        glob.glob("src/glitxh/*.py"),
+        glob.glob("src/n4tazac/*.py"),
         compiler_directives={
             "language_level": "3"
         }
